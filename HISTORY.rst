21a0dd9748384b85fdc1f2101dec741769bdf7d5
necusjz <necusjz@gmail.com>
2022-04-18 10:46:54 +0800

    update scripts

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,21 +3,6 @@
 Release History
 ===============
 
-0.15.0
+0.1.0
 ++++++
-* Initial release
-* Initial release
-* Initial release
-* Initial release
-
-0.7.0
-++++++
-* Initial release
-
-0.6.0
-++++++
-* Initial release
-
-0.5.0
-++++++
-* Initial release
+* Initial release.
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ aazdev_path = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(aazdev_path, "aazdev", "version.py"), "r") as version_file:
     exec(version_file.read(), VERSION)
 
-with open("README.md", "r", encoding="utf-8") as fp:
+with open("README.rst", "r", encoding="utf-8") as fp:
     README = fp.read()
 with open("HISTORY.rst", "r", encoding="utf-8") as fp:
     HISTORY = fp.read()
@@ -67,6 +67,6 @@ setup(
     install_requires=read_requirements("requirements.txt"),
     python_requires=">=3.6",
     entry_points={
-        'console_scripts': ['aazdev=aazdev.backend.main:main']
+        "console_scripts": ["aazdev=aazdev.backend.main:main"]
     },
 )
